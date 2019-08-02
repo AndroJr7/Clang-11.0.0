6c4a9e275740996de85afb9669db6c714cabbcdb
Yousef Algadri <yusufgadrie@gmail.com>
2019-08-02 18:32:50 +0700

    clang: Enable ThinLTO suuport
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- include/llvm/Support/VCSRevision.h
+++ include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "175f0930907c56a80e0500cd201910c36222e788"
+#define LLVM_REVISION "b874b3d3faac98b7fe807ffec0050d452a6a44b6"
 #define LLVM_REPOSITORY "git://github.com/llvm/llvm-project"
