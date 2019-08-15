151054768babb9c48fa2cb37d2d0d0cbd91c3a78
Yousef Algadri <yusufgadrie@gmail.com>
2019-08-15 19:32:49 +0700

    clang: Update as of 2019-08-15
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- include/llvm/Support/VCSRevision.h
+++ include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "8c5c09780d4981995927ca06572e833b1b8116f6"
+#define LLVM_REVISION "e1bc32bddf2edd1c91154f397f2ba07920d8330d"
 #define LLVM_REPOSITORY "git://github.com/llvm/llvm-project"
